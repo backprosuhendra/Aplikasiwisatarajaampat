f79250f7b8d03db9304dce93baa24bc0911b8e98
Elfan ulil Albab <102909928+elfan03@users.noreply.github.com>
2023-10-13 16:23:22 +0900

    rubah elfan test

---
+++ app/src/main/java/com/example/aplikasiwisatarajaampat/Pemesanan_kamar.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasiwisatarajaampat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pemesanan_kamar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pemesanan_kamar)
+    }
+}
