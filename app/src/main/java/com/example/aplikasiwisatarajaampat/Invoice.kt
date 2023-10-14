0e912c712e86e03a26d94f5a296c312bcfcb9922
Elfan ulil Albab <102909928+elfan03@users.noreply.github.com>
2023-10-15 00:56:46 +0900

    rubah malam minggu tes

---
+++ app/src/main/java/com/example/aplikasiwisatarajaampat/Invoice.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasiwisatarajaampat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Invoice : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invoice)
+    }
+}
