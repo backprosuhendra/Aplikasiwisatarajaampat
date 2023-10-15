c0db397c62ec445d93cf4ad68fd14dfa2f9f0ee4
Elfan ulil Albab <102909928+elfan03@users.noreply.github.com>
2023-10-15 18:01:47 +0900

    rubah hari minggu sore tes

--- app/src/main/java/com/example/aplikasiwisatarajaampat/splash.kt
+++ app/src/main/java/com/example/aplikasiwisatarajaampat/splash.kt
@@ -1,11 +1,23 @@
 package com.example.aplikasiwisatarajaampat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash)
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000)
+            val intent = Intent(this@splash, home::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
