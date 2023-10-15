7e716c29355d8c45a7b827f92d86ba225e95bdf1
Elfan ulil Albab <102909928+elfan03@users.noreply.github.com>
2023-10-16 00:18:56 +0900

    rubah hari minggu maalam  tes

---
+++ app/src/main/java/com/example/aplikasiwisatarajaampat/instruksipembayaran.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasiwisatarajaampat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class instruksipembayaran : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instruksipembayaran)
+    }
+}
