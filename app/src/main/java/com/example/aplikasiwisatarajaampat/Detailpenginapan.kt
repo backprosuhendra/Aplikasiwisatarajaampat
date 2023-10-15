5321548e1299192b7e9fc3ff21713e8147956552
Elfan ulil Albab <102909928+elfan03@users.noreply.github.com>
2023-10-15 23:06:10 +0900

    rubah hari minggu maalam  tes

--- app/src/main/java/com/example/aplikasiwisatarajaampat/Detailpenginapan.kt
+++ app/src/main/java/com/example/aplikasiwisatarajaampat/Detailpenginapan.kt
@@ -3,9 +3,9 @@ package com.example.aplikasiwisatarajaampat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Detailagent : AppCompatActivity() {
+class Detailpenginapan : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detailagent)
+        setContentView(R.layout.activity_detailpenginapan)
     }
 }
