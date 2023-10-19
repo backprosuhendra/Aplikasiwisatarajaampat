e160747fa49a79b8644c7e228f5548e43e3c6cdf
Elfan ulil Albab <102909928+elfan03@users.noreply.github.com>
2023-10-20 00:44:21 +0900

    elfan perbaiki

--- app/src/main/java/com/example/aplikasiwisatarajaampat/onboarding1.kt
+++ app/src/main/java/com/example/aplikasiwisatarajaampat/onboarding1.kt
@@ -3,9 +3,9 @@ package com.example.aplikasiwisatarajaampat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class onboarding4 : AppCompatActivity() {
+class onboarding1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding4)
+        setContentView(R.layout.activity_onboarding1)
     }
 }
