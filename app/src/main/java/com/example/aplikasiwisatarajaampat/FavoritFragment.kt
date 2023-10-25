2244ffe3bdf8f1082d93b11938e4dd71dda59a5c
Elfan ulil Albab <102909928+elfan03@users.noreply.github.com>
2023-10-25 21:31:28 +0900

    elfan buton nav

--- app/src/main/java/com/example/aplikasiwisatarajaampat/FavoritFragment.kt
+++ app/src/main/java/com/example/aplikasiwisatarajaampat/FavoritFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.aplikasiwisatarajaampat.databinding.CostumtoolbarBinding
 
 class FavoritFragment : Fragment() {
 
--- app/src/main/java/com/example/aplikasiwisatarajaampat/activity_Halaman_Login.kt
+++ app/src/main/java/com/example/aplikasiwisatarajaampat/activity_Halaman_Login.kt
@@ -3,7 +3,7 @@ package com.example.aplikasiwisatarajaampat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class activity_Halaman_Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_halaman_login)
